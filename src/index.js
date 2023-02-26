0d2860897f9036f80869bbf5e99b40b337a19e03
Fidel Ernesto <fidehlg89@gmail.com>
2023-02-26 16:13:05 -0500

    fix: new port

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
